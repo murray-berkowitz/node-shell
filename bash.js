875343368371c21b4c801994e107d2274188a285
Murray Berkowitz <murray.berkowitz93@gmail.com>
2017-07-09 21:50:32 -0400

    completed cat/head/tail/sort/wc

--- bash.js
+++ bash.js
@@ -1,32 +1,18 @@
-// why isn't write in the methods we logged
-// outut a prompt
 
 var commands = require('./commands');
-//var fs = require('fs')
-
 
 process.stdout.write('prompt >');
 
 process.stdin.on('data', function(data){
   var cmd = data.toString().trim().split(" ");
-  //var cmd = data.toString().trim();
-  //var func = cmd[0];
-  
   function spaceDelimited(array) {
-      var current;
       var func = commands[array[0]];
       var args = array.slice(1);
-      func(current,args);
+      func(args);
   }
-
   spaceDelimited(cmd);
-  //commands[func]();
-
-  process.stdout.write('You typed: ' + data);
-  process.stdout.write('\nprompt >');
+  process.stdout.write('You typed: ' + data+ '\n');
+  //process.stdout.write('\nprompt >');
 
 });
 
-
-//console.log(process)
-//setTimeout(function(){console.log('hey')}, 4000)
--- commands.js
+++ commands.js
@@ -1,29 +1,146 @@
 var fs = require('fs')
-
-var listFiles = function(input, args){
-    fs.readdir('.', function(err, files){
+var listFiles = function(args){
+  fs.readdir('.', function(err, files){
     if (err) throw err;
+    var async_counter = files.length;
     files.forEach(function(file){
       process.stdout.write(file.toString() + "\n");
+      async_counter--;
+      if(async_counter == 0){
+        process.stdout.write("prompt > ");
+      }
     })
-    process.stdout.write("prompt > ");
+  })
+}
+var head = function(files){
+  var async_counter = files.length;
+  files.forEach(function(file){
+    fs.readFile(file, function(err, data){
+      if(err){
+        throw err;
+      }
+      else {
+        data = data.toString().split('\n');
+        var line = 0;
+        while(line < 10){
+          console.log(data[line]);
+          line++;
+        }
+        async_counter--;
+        if(async_counter == 0){
+          process.stdout.write("prompt > ");
+        }
+      }
+    })
+  })
+};
+
+var sort = function(files){
+  var async_counter = files.length;
+  files.forEach(function(file){
+    fs.readFile(file, function(err, data){
+      if(err){
+        throw err;
+      }
+      else {
+        data = data.toString().split('\n');
+        var line = 0;
+        while(line < data.length){
+          console.log(data[line].split(' ').sort().join(' '));
+          line++;
+        }
+        async_counter--;
+        if(async_counter == 0){
+          process.stdout.write("prompt > ");
+        }
+      }
+    })
+  })
+};
+
+var wc = function(files){
+  var async_counter = files.length;
+  files.forEach(function(file){
+    fs.readFile(file, function(err, data){
+      if(err){
+        throw err;
+      }
+      else {
+        data = data.toString().split('\n');
+        console.log(data.length);
+        async_counter--;
+        if(async_counter == 0){
+          process.stdout.write("prompt > ");
+        }
+      }
+    })
+  })
+};
+
+var tail  = function(files){
+  var async_counter = files.length;
+  files.forEach(function(file){
+    fs.readFile(file, function(err, data){
+      if(err){
+        throw err;
+      }
+      else {
+        data = data.toString().split('\n');
+        var line = data.length-10;
+        while(line < (data.length-1)){
+          console.log(data[line]);
+          line++;
+        }
+        async_counter--;
+        if(async_counter == 0){
+          process.stdout.write("prompt > ");
+        }
+      }
+    })
+  })
+};
+var displayFiles = function(files){
+  var async_counter = files.length;
+  files.forEach(function(file){
+    fs.readFile(file, function(err, data){
+      if(err){
+        throw err;
+      }
+      else {
+        console.log(data.toString());
+        async_counter--;
+        if(async_counter == 0){
+          process.stdout.write("prompt > ");
+        }
+      }
+    })
   })
 }
 
-
-var echoFunc = function(input, args){
+var echoFunc = function(args){
   var string = args.join(' ');
   process.stdout.write(string+'\n');
 }
 
 
 module.exports = {
-  pwd: function(input, args){
-    console.log(process.cwd())
+  pwd: function(args){
+    console.log(process.cwd());
+    setTimeout(function(){
+      process.stdout.write("prompt > ");
+    },0);
   },
-  date: function(input, args){
-    console.log(new Date(Date.now()))
+  date: function(args){
+    console.log(new Date(Date.now()));
+    setTimeout(function(){
+      process.stdout.write("prompt > ");
+    },0);
   },
   ls: listFiles,
-  echo: echoFunc
+  echo: echoFunc,
+  cat: displayFiles,
+  head:head,
+  tail:tail,
+  sort:sort,
+  wc:wc
 }
